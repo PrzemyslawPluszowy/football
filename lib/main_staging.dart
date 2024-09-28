a98c9b9f1907c21b9657fbfc1390203a58f674c3
Przemysław Nowak <misiek440@gmail.com>
2024-09-28 11:54:31 +0200

    link

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
 import 'package:football/bootstrap.dart';
 import 'package:football/core/di/di.dart';
 import 'package:football/features/app/app.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
+late final Talker talker;
 void main() {
+  talker = TalkerFlutter.init();
   setupLocator();
-  bootstrap(App.new);
+  runZonedGuarded(
+    () => bootstrap(App.new),
+    (Object error, StackTrace stack) {
+      talker.handle(error, stack, 'Uncaught app exception');
+    },
+  );
 }
