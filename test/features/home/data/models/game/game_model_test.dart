0373ffdb50bf23c7226daea1f56a1e43f9090109
Przemysław Nowak <misiek440@gmail.com>
2024-10-16 19:26:41 +0200

    poprawka async twst

--- test/features/home/data/models/game/game_model_test.dart
+++ test/features/home/data/models/game/game_model_test.dart
@@ -1,32 +1,16 @@
-// [
-//   {
-//     "id": 0,
-//     "date": "2024-09-14T17:15:07.000000Z",
-//     "score": "1:0",
-//     "home_club": {
-//       "id": 0,
-//       "name": "LKS Pisarowce",
-//       "stadium": "233, 38-530 Pisarowce"
-//     },
-//     "away_club": {
-//       "id": 0,
-//       "name": "LKS Pisarowce",
-//       "stadium": "233, 38-530 Pisarowce"
-//     },
-//     "created_at": "2024-09-14T17:15:07.000000Z"
-//   }
-// ]
-
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:football/features/home/data/models/game/game_model.dart';
 
-void main() async {
-  final file = File('test/features/home/data/models/game/game_response.json');
-  final mockJson =
-      json.decode(await file.readAsString()) as Map<String, dynamic>;
+void main() {
+  late Map<String, dynamic> mockJson;
+
+  setUpAll(() async {
+    final file = File('test/features/home/data/models/game/game_response.json');
+    mockJson = json.decode(await file.readAsString()) as Map<String, dynamic>;
+  });
 
   final testGameModel = GameModel(
     id: 0,
@@ -45,13 +29,13 @@ void main() async {
     createdAt: DateTime(2024),
   );
 
-  test('should convert from model to json', () async {
+  test('should convert from model to json', () {
     final result = testGameModel.toJson();
 
     expect(result, mockJson);
   });
 
-  test('should convert from json to model', () async {
+  test('should convert from json to model', () {
     final result = GameModel.fromJson(mockJson);
 
     expect(result, isA<GameModel>());
