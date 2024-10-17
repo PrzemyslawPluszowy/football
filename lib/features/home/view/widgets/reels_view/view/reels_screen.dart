90be11190ee3d6df8bba9024a27ccf7052e72d0f
Przemysław Nowak <misiek440@gmail.com>
2024-10-17 15:44:26 +0200

    videoplayer WIP

--- lib/features/home/view/widgets/reels_view/view/reels_screen.dart
+++ lib/features/home/view/widgets/reels_view/view/reels_screen.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:football/common/widgets/cashed_image.dart';
 import 'package:football/features/home/domain/entities/reeel_entity.dart';
+import 'package:video_player/video_player.dart';
 
 @RoutePage()
 class ReelsScreen extends StatelessWidget {
@@ -16,14 +19,63 @@ class ReelsScreen extends StatelessWidget {
       child: PageView.builder(
         controller: pageController,
         physics: const BouncingScrollPhysics(),
-        scrollBehavior: const MaterialScrollBehavior(),
+        scrollDirection: Axis.vertical,
         itemCount: reels.length,
-        itemBuilder: (context, index) => Center(
-          child: Text(
-            reels[index].title,
-            style: const TextStyle(fontSize: 24),
-          ),
+        itemBuilder: (context, index) {
+          return SamplePlayer(reel: reels, index: index);
+        },
+      ),
+    );
+  }
+}
+
+class SamplePlayer extends StatefulWidget {
+  const SamplePlayer({required this.reel, required this.index, super.key});
+
+  final List<Reel> reel;
+  final int index;
+
+  @override
+  SamplePlayerState createState() => SamplePlayerState();
+}
+
+class SamplePlayerState extends State<SamplePlayer> {
+  late FlickManager flickManager;
+  late bool isVideo = widget.reel[widget.index].videoUrls != null;
+
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(
+      videoPlayerController: VideoPlayerController.networkUrl(
+        Uri.parse(
+          widget.reel[widget.index].videoUrls?.small ?? '',
         ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isVideo) {
+      return Container(
+        child: CashedImage(
+          imageUrl: widget.reel[widget.index].imageUrls?.large.url ?? '',
+          width: MediaQuery.of(context).size.width.toInt(),
+          height: MediaQuery.of(context).size.height.toInt(),
+        ),
+      );
+    }
+    return Container(
+      child: FlickVideoPlayer(
+        flickVideoWithControlsFullscreen: const SizedBox(),
+        flickManager: flickManager,
       ),
     );
   }
